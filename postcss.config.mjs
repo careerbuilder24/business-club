229ff0672d9147e4099c3ca7cb822023edc42238
careerbuilder24 <careerbuilder.com.bd@gmail.com>
2025-10-20 17:56:49 +0600

    fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,21 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
+
+// export default config
+// postcss.config.mjs
+import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/postcss';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss,
+    autoprefixer,
   },
-}
+};
 
-export default config
+export default config;
